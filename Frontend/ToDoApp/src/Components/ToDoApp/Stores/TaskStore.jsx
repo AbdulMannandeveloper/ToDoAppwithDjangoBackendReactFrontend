d90ec11a0b4aeb386aaae3c062e6ac2e3c68277a
AbdulMannanDeveloper <mannanag13@gmail.com>
2026-01-13 16:31:33 +0500

    Add Action for Editing Tasks in Task Store

--- Frontend/ToDoApp/src/Components/ToDoApp/Stores/TaskStore.jsx
+++ Frontend/ToDoApp/src/Components/ToDoApp/Stores/TaskStore.jsx
@@ -4,39 +4,79 @@ import axios from 'axios';
 const BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/';
 const API_URL = `${BASE_URL}api/todo/`;
 
-const TaskStore = create(
-    set({
-        tasks : {},
+const useTaskStore = create(
+    (set,get) => ({
+        tasks : [],
         isLoading : false,
         error : null,
 
-        fetchData : async() => set(
-            (state) => {
-                set({isLoading: true});
-                try{
-                    const token = localStorage.getItem('access_token');
-                    const response = axios.get(`${API_URL}read_task/`, {
-                        headers= {Authorization: `Bearer ${token}`}
-                    });
+        fetchTasks : async() => {
+            set({isLoading: true});
+            try{
+                const token = localStorage.getItem('access_token');
+                const response = await axios.get(`${API_URL}read_task/`, {
+                    headers: {Authorization: `Bearer ${token}`}
+                });
 
-                    set({tasks: response.data}, isLoading = false);
-                    
-                }catch(err){
-                    set({error: err.message, isLoading: false});
-                }
-            }
-        )
-
-
-        addTask : async() => set(
-            (state) => {
+                set({tasks: response.data, isLoading : false});
                 
+            }catch(err){
+                set({error: err.message, isLoading: false});
             }
-        )
+        },
 
-        
+        addTask : async(taskTitle) =>{
+            set({isLoading: true});
+            try{
+                const token = localStorage.getItem('access_token');
+                const response = await axios.post(`${API_URL}create_task`, 
+                    { task: taskTitle },
+                    { headers: {Authorization: `Bearer ${token}`} },
+                );
+
+                await get().fetchTasks();
+
+                set({ isLoading: false });
+                return true;
+
+
+            }catch(err){
+                const errorMessage = err.response?.data?.error || "Failed to add task";
+                set({ error: errorMessage, isLoading: false });
+                return false;
+            }
+
+        },
+
+        editTask : async(task_id, taskTitle, is_done_status) =>{
+            set({isLoading: true});
+            try{
+                const token = localStorage.getItem('access_token');
+                const response = await axios.put(`${API_URL}update_task`, 
+                    { 
+                        task: taskTitle,
+                        task_id: task_id,
+                        is_done: is_done_status
+                    },
+                    { headers: {Authorization: `Bearer ${token}`} },
+                );
+
+                await get().fetchTasks();
+
+                set({ isLoading: false });
+                return true;
+
+
+            }catch(err){
+                const errorMessage = err.response?.data?.error || "Failed to update task";
+                set({ error: errorMessage, isLoading: false });
+                return false;
+            }
+
+        },
+
 
     })
-)
+);
 
-export default TaskStore;
+export default useTaskStore;
