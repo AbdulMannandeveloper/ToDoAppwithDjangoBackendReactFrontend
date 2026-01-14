967b4b6f0cfce6a500d0cd9d4872e78b01aba2a5
AbdulMannanDeveloper <mannanag13@gmail.com>
2026-01-14 11:09:53 +0500

    Create App.jsx file

--- Frontend/ToDoApp/src/App.jsx
+++ Frontend/ToDoApp/src/App.jsx
@@ -1,9 +1,51 @@
-function App() {
-  return (
-    <>
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-    </>
+import Login from './Components/Authentication/Login';
+import SignUp from './Components/Authentication/SignUp';
+import ChangePassword from './Components/Authentication/ChangePassword';
+import Dashboard from './Components/ToDoApp/dashboard';
+
+import useAuthStore from './Components/Authentication/Stores/AuthStore';
+
+function App() {
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        
+        <Route 
+            path="/" 
+            element={<Navigate to={isAuthenticated ? "/dashboard" : "/login"} />} 
+        />
+
+        <Route 
+            path="/login" 
+            element={!isAuthenticated ? <Login /> : <Navigate to="/dashboard" />} 
+        />
+        <Route 
+            path="/signup" 
+            element={!isAuthenticated ? <SignUp /> : <Navigate to="/dashboard" />} 
+        />
+
+        <Route 
+            path="/dashboard" 
+            element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />} 
+        />
+        
+        <Route 
+            path="/change-password" 
+            element={isAuthenticated ? <ChangePassword /> : <Navigate to="/login" />} 
+        />
+
+        <Route 
+            path="*" 
+            element={<Navigate to={isAuthenticated ? "/dashboard" : "/login"} />} 
+        />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
