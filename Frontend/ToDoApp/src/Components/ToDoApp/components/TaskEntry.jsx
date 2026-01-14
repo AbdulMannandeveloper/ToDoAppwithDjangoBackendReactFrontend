6f7d1430af0d4670b65f0f1a8e143f3f8876c652
AbdulMannanDeveloper <mannanag13@gmail.com>
2026-01-14 09:50:30 +0500

    Implement Task Entry Component for Adding Tasks

--- Frontend/ToDoApp/src/Components/ToDoApp/components/TaskEntry.jsx
+++ Frontend/ToDoApp/src/Components/ToDoApp/components/TaskEntry.jsx
@@ -1,4 +1,38 @@
 import useTaskStore from "./Stores/TaskStore";
 import { useState } from  'react'
 
-def 
+const TaskEntry = () => {
+    const [input, setInput] = useState('');
+    const setTask = useTaskStore((state)=> state.addTask);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(!(input.trim())) return;
+        addTask(input);
+
+        setInput('');
+
+    }
+
+    return (
+        <>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        type="text"
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        placeholder="Enter Your Task"
+                    />
+                    <button 
+                        type="submit"
+                    >Add Task</button>
+                </form>
+            </div>
+        </>
+    )
+
+}
+
+export default TaskEntry;
